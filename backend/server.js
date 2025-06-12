b1b5c4ea607f129728b9dd0b61e2b469ac587993
Prashant Shrestha <161279461+prashantStha308@users.noreply.github.com>
2025-06-13 00:03:50 +0545

    preparing for launch

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,11 @@ import { config } from "dotenv";
 import userRouter from "./routes/users.route.js"
 import trackRouter from "./routes/track.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
+import { fileURLToPath } from 'url';
+import path from "path";
 
 // models
+// idk why,if import garena bhane won't sent data
 import Artist from "./models/artist.model.js";
 import User from "./models/user.model.js";
 import Playlist from "./models/playlist.model.js";
@@ -15,12 +18,25 @@ import Album from "./models/album.model.js";
 config();
 const app = express();
 
+// resolving correct path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.resolve(path.dirname(__filename), '..');  // <-- corrected to point to project root
+
 app.use(express.json());
 app.use(cors());
 
 app.use( '/api/user' , userRouter );
 app.use( '/api/track' , trackRouter );
 app.use( '/api/playlist' , playlistRouter );
+
+// production setup for serving frontend build
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    });
+}
 
 app.listen( process.env.PORT , ()=>{
     console.log(`Server started at http://localhost:${process.env.PORT}`);
